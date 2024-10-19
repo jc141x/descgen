a0c889432e924b0465ca17f1930b880d0affce0f
hackernoodle <118927624+hackernoodle@users.noreply.github.com>
2024-10-19 12:35:54 +0300

    update template

--- models/Entity.php
+++ models/Entity.php
@@ -116,7 +116,10 @@ class Entity
             {$this->desc}
 
             [size=14][url=https://gitlab.com/jc141x/portal/]SETUP AND SUPPORT[/url][/size] - Setup is not optional.
-            Game reqs
+            System requirements
+            A GNU/Linux OS based on Arch (EndeavourOS, Artix, CachyOS), Debian Sid (Sparky Rolling, Siduction) or Fedora (Ultramarine, Nobara).
+            Vulkan capable GPU/APU
+
             {$this->reqs}
             
             Other info
@@ -126,16 +129,16 @@ class Entity
             Gamepad support: Confirmed/Not Working/Could not verify/Not necessary for this game
 
             Integrated features
-            - Play without extracting, highly efficient usage of space through the use of the DwarFS compression filesystem.
-            
-            - All network activity in the game is blocked by default; no data is sent back to any third party.
-            
-            - Filesystem isolation for the game files through the use of Bubblewrap. All user data is read-only to the game when it is run.
-            
-            - Gamescope support. Adds the ability to enable additional features such as FSR, NIS, HDR and other options used on SteamDeck.
-            
-            - Global and local default configurations are available to users to pick and choose which features to enable or not. (as well as general behavior)
-                        
+            Run with no extraction due to the Dwarfs image format. Mods/configs can be added through overlay-storage directory.
+
+            Filesystem isolation for the game files through the use of Bubblewrap. Executables have read-only access to the system except for /dev /tmp.
+
+            All network activity of the executable blocked by default with Bubblewrap isolation. Useful for privacy and security concerns.
+
+            Gamescope support. Scales to desktop resolution, isolates game from display server and provides many useful features.
+
+            Global and local default configurations are available to users to pick which features to enable or not.
+
             [img]{$this->screen[0]}[/img]
             [img]{$this->screen[1]}[/img]
             [img]{$this->screen[2]}[/img]
